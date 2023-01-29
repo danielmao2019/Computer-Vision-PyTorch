278b4513b49c6e8da75c6cc01f58208e4207a40b
Daniel Mao <danielmao2019@gmail.com>
2023-01-29 15:31:17 -0500

    [LeNet] Overfit LeNet on fake data

--- data/datasets/Overfit/OverfitDataset.py
+++ data/datasets/Overfit/OverfitDataset.py
@@ -4,14 +4,17 @@ import torch
 
 class OverfitDataset(Dataset):
 
-    TASK_OPTIONS = ['image_classification']
+    TASK_OPTIONS = ['image_classification', 'semantic_segmentation']
 
-    def __init__(self, task):
-        self.image = torch.rand(3, 224, 224)
+    def __init__(self, task, image_shape, label_shape):
+        self.image = torch.rand(image_shape)
+        self.label = torch.zeros(size=label_shape)
         if task not in self.TASK_OPTIONS:
             raise NotImplementedError()
-        if task == 'image_classification':
-            self.label = 0
+        if task in ['image_classification', 'semantic_segmentation']:
+            self.label = self.label.type(torch.int64)
+        else:
+            raise RuntimeError(f"Argument {task=} not handled properly.")
         self.core = [(self.image, self.label)]
 
     def __getitem__(self, idx):
