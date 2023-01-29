ef463e9cfe53f6576af80cc322d4f31e815c5e3b
Daniel Mao <81886629+danielmao2019@users.noreply.github.com>
2023-01-29 15:35:20 -0500

    [Lenet] Create Lenet.py (#4)
    * [LeNet] Create LeNet.py
    * [Datasets] Refactored OverfitDataset
    * [Training] Create train_model_minimal.py

--- data/datasets/Overfit/test_OverfitDataset.py
+++ data/datasets/Overfit/test_OverfitDataset.py
@@ -3,11 +3,12 @@ import torch
 import data
 
 
-@pytest.mark.parametrize("task", [
-    pytest.param('image_classification'),
+@pytest.mark.parametrize("task, input_shape, output_shape", [
+    pytest.param('image_classification', (1, 224, 224), (10,)),
+    pytest.param('image_classification', (3, 224, 224), (10,)),
 ])
-def test_OverfitDataset(task):
-    dataset = data.datasets.OverfitDataset(task=task)
+def test_OverfitDataset(task, input_shape, output_shape):
+    dataset = data.datasets.OverfitDataset(task=task, image_shape=input_shape, label_shape=output_shape)
     assert len(dataset) == 1
     example = next(iter(dataset))
     assert type(example) == tuple
@@ -15,7 +16,8 @@ def test_OverfitDataset(task):
     image, label = example
     # check image
     assert type(image) == torch.Tensor
-    assert image.shape == (3, 224, 224)
+    assert image.shape == input_shape
     assert image.dtype == torch.float32
     # check label
-    assert type(label) == int
+    assert type(label) == torch.Tensor
+    assert label.shape == output_shape
---
+++ training/__init__.py
@@ -0,0 +1,9 @@
+"""
+TRAINING APIs
+"""
+from training.train_model_minimal import train_model_minimal
+
+
+__all__ = (
+    "train_model_minimal",
+)
