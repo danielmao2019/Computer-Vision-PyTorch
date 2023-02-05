36317c1e88b0aac058115edc5cb55757e2ba55c7
Daniel Mao <danielmao2019@gmail.com>
2023-02-04 22:04:43 -0500

    [Evaluation] Create eval_model.py

---
+++ evaluation/__init__.py
@@ -0,0 +1,9 @@
+"""
+EVALUATION APIs.
+"""
+from evaluation.eval_model import eval_model
+
+
+__all__ = (
+    "eval_model",
+)
---
+++ evaluation/eval_model.py
@@ -0,0 +1,13 @@
+from tqdm import tqdm
+
+
+def eval_model(model, dataloader, metrics, device):
+    model.eval()
+    tot_scores = [0] * len(metrics)
+    for element in tqdm(dataloader):
+        image, label = element[0].to(device), element[1].to(device)
+        output = model(image)
+        for idx, metric in enumerate(metrics):
+            tot_scores[idx] += metric(output, label)
+    avg_scores = [tot_scores[idx] / len(dataloader) for idx in range(len(tot_scores))]
+    return avg_scores
