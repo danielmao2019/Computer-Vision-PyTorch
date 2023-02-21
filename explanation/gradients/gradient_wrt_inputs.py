f7562c7fdccdd5fbd625ac2cb8e118e0c1ac6925
Daniel Mao <danielmao2019@gmail.com>
2023-02-21 17:20:50 -0500

    f

--- explanation/gradients/gradient_wrt_inputs.py
+++ explanation/gradients/gradient_wrt_inputs.py
@@ -9,23 +9,19 @@ import models
 import explanation
 
 
-def tanh_backward(inputs):
-    def new_layer(x):
-        assert x.shape == inputs.shape
-        return x * explanation.gradients.tanh_gradient(inputs)
-    return new_layer
-
-
 class GradientModelInputs(torch.nn.Module):
 
     def __init__(self, model, layer_idx):
         super(GradientModelInputs, self).__init__()
         model.eval()
+        self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
         self.model = model
         self.layer_idx = layer_idx
-        self.length = len(list(model.children()))
-        self.memory = [None] * length
-        self.hooks = explanation.gradients.hooks.register_hooks(model=self.model, memory=self.memory)
+        self.length = len(list(self.model.children()))
+        self.memory = [None] * self.length
+        self.hooks = explanation.gradients.hooks.register_hooks(
+            model=self.model, memory=self.memory, layer_idx=layer_idx,
+        )
 
     def forward(self, images, labels, criterion_gradient_func):
         outputs = self.model(images)
@@ -33,35 +29,36 @@ class GradientModelInputs(torch.nn.Module):
         assert len(gradients.shape) == 2, f"{gradients.shape=}"
         for idx in range(self.length-1, self.layer_idx-1, -1):
             layer = list(self.model.children())[idx]
-            new_layer = self.get_new_layer(layer)
+            new_layer = self.get_new_layer(layer, idx)
             gradients = new_layer(gradients)
         return gradients
 
-    def get_new_layer(self, layer):
+    def get_new_layer(self, layer, idx):
         if type(layer) == torch.nn.Conv2d:
-            new_weight = layer.weight.data.clone()
-            assert len(new_weight.shape) == 4
-            new_weight = torch.flip(new_weight, dims=[2, 3])
-            new_weight = torch.stack([new_weight[:, k, :, :] for k in range(new_weight.shape[1])], dim=0)
+            new_weights = layer.weight.data.clone()
+            assert len(new_weights.shape) == 4
+            new_weights = torch.flip(new_weights, dims=[2, 3])
+            new_weights = torch.stack([new_weights[:, k, :, :] for k in range(new_weights.shape[1])], dim=0)
             new_layer = torch.nn.Conv2d(
-                in_channels=new_weight.shape[1], out_channels=new_weight.shape[0],
-                kernel_size=(new_weight.shape[2], new_weight.shape[3]), stride=1,
-                padding=(new_weight.shape[2]-1, new_weight.shape[3]-1), bias=False,
+                in_channels=new_weights.shape[1], out_channels=new_weights.shape[0],
+                kernel_size=(new_weights.shape[2], new_weights.shape[3]), stride=1,
+                padding=(new_weights.shape[2]-1, new_weights.shape[3]-1), bias=False,
             )
-            new_layer.weights = torch.nn.Parameter(new_weight)
+            new_layer.weights = torch.nn.Parameter(new_weights)
         elif type(layer) == torch.nn.Linear:
-            new_weight = layer.weight.data.clone()
-            assert len(new_weight.shape) == 2
-            new_weight = torch.permute(new_weight, dims=(1, 0))
+            new_weights = layer.weight.data.clone()
+            assert len(new_weights.shape) == 2
+            new_weights = torch.permute(new_weights, dims=(1, 0))
             new_layer = torch.nn.Linear(
-                in_features=new_weight.shape[1], out_features=new_weight.shape[0], bias=False,
+                in_features=new_weights.shape[1], out_features=new_weights.shape[0], bias=False,
             )
-            new_layer.weights = torch.nn.Parameter(new_weight)
+            new_layer.weights = torch.nn.Parameter(new_weights)
         ##################################################
         # pooling layers
         ##################################################
         elif type(layer) == torch.nn.AvgPool2d:
             kernel_size = layer.kernel_size
+            print(f"{kernel_size=}")
             new_layer = lambda x: torch.tile(x, dims=(2 ,2)) / kernel_size**2
         elif type(layer) == models.layers.GlobalAveragePooling2D:
             input_size = self.memory[idx]
@@ -74,19 +71,28 @@ class GradientModelInputs(torch.nn.Module):
         elif type(layer) == torch.nn.Tanh:
             inputs = self.memory[idx]
             assert inputs is not None, f"{idx=}"
-            new_layer = tanh_backward(inputs)
+            new_layer = self.tanh_backward(inputs)
         elif type(layer) == torch.nn.Softmax:
             outputs = self.memory[idx]
-            assert outputs is not None
-            assert len(outputs.shape) == 2, f"{outputs.shape=}"
-            new_weight = torch.outer(outputs, outputs) - torch.diag(outputs)
-            assert len(new_weight.shape) == 2
-            new_layer = torch.nn.Linear(
-                in_features=len(outputs), out_features=len(outputs), bias=False,
-            )
-            new_layer.weights = torch.nn.Parameter(new_weight)
+            assert outputs is not None, f"{idx=}"
+            new_layer = self.softmax_backward(outputs)
         else:
             raise NotImplementedError(f"[ERROR] Layers of type {type(layer)} not implemented.")
         if isinstance(new_layer, torch.nn.Module):
             new_layer.eval()
-            new_layer = new_layer.to(device)
+            new_layer = new_layer.to(self.device)
+
+    def tanh_backward(self, inputs):
+        def new_layer(x):
+            assert x.shape == inputs.shape
+            return x * explanation.gradients.tanh_gradient(inputs)
+        return new_layer
+
+    def softmax_backward(self, outputs):
+        def new_layer(x):
+            assert x.shape == outputs.shape
+            new_weights = (torch.bmm(torch.unsqueeze(outputs, dim=1), torch.unsqueeze(outputs, dim=2))
+                           - torch.diag_embed(outputs))
+            assert new_weights.shape == (outputs.shape[0], outputs.shape[1], outputs.shape[1])
+            return torch.bmm(new_weights, torch.unsqueeze(x, dim=2))
+        return new_layer
--- explanation/gradients/hooks.py
+++ explanation/gradients/hooks.py
@@ -10,6 +10,8 @@ def define_hook(memory, idx):
         if type(module) == models.layers.GlobalAveragePooling2D:
             assert len(input.shape) == 4, f"{input.shape=}"
             memory[idx] = input.shape[2:4]
+            assert type(memory[idx]) == tuple
+            assert len(memory[idx]) == 2
         elif type(module) in [torch.nn.ReLU, torch.nn.Tanh]:
             memory[idx] = input.detach()
         elif type(module) == torch.nn.Softmax:
@@ -20,11 +22,12 @@ def define_hook(memory, idx):
     return hook
 
 
-def register_hooks(model, memory):
-    hooks = [None] * len(list(model.children()))
+def register_hooks(model, memory, layer_idx=0):
+    length = len(list(model.children()))
+    hooks = [None] * length
     assert len(memory) == len(hooks)
-    for idx, layer in enumerate(list(model.children())):
-        hooks[idx] = layer.register_forward_hook(define_hook(memory, idx))
+    for idx in range(layer_idx, length):
+        hooks[idx] = list(model.children())[idx].register_forward_hook(define_hook(memory, idx))
     return hooks
 
 
