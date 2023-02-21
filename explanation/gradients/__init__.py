5affc5b34d0d804f8873f85dfe9acf75954b7373
Daniel Mao <danielmao2019@gmail.com>
2023-02-21 17:25:58 -0500

    f

--- explanation/gradients/__init__.py
+++ explanation/gradients/__init__.py
@@ -1,13 +1,13 @@
 """
-GRADIENT APIs.
+GRADIENTs APIs.
 """
-from explanation.gradients.gradients import tanh_gradient, CE_gradient, MSE_gradient
 from explanation.gradients import hooks
-from explanation.gradients.gradient_wrt_inputs import get_backward_model
+from explanation.gradients.gradients import tanh_gradient, CE_gradient, MSE_gradient
+from explanation.gradients.GradientModelInputs import GradientModelInputs
 
 
 __all__ = (
     "hooks",
     "tanh_gradient", "CE_gradient", "MSE_gradient",
-    "get_backward_model",
+    "GradientModelInputs",
 )
