c23d7a21b19591b369e7116c93756f1cf40dee4f
Daniel Mao <danielmao2019@gmail.com>
2023-03-12 22:07:17 -0400

    f

--- utils/plot/inner_products_distribution.py
+++ utils/plot/inner_products_distribution.py
@@ -2,18 +2,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 import glob
 import os
+from tqdm import tqdm
 
 
-for filepath in glob.glob("saved_models/LeNet_MNIST_Multi_4/tensors/inner_products_weights/inner_products*.txt"):
+filepath_list = sorted(glob.glob("saved_models/LeNet_MNIST_Multi_3/tensors/inner_products_weights/inner_products_checkpoint*.pt.txt"))
+for filepath in tqdm(filepath_list):
     inner_products = np.loadtxt(filepath)
-    plt.figure()
-    plt.ylim([0, 60000])
-    plt.xlabel("Inner Product")
-    plt.ylabel("Frequency")
-    plt.hist(inner_products, bins=100)
+    fig, ax_list = plt.subplots(nrows=1, ncols=2, figsize=(20, 10))
+    for ax in ax_list:
+        ax.set_ylim([0, 60000])
+        ax.set_xlabel("Inner Product")
+        ax.set_ylabel("Frequency")
+    ax_list[0].hist(inner_products, bins=100, range=[-100, +100])
+    ax_list[1].hist(inner_products, bins=100, range=[-0.1, +0.1])
     epoch = filepath.split('/')[-1].split('.')[0].split('_')[-1]
-    plt.title(f"Histogram of Inner Products on Training Set" + '\n' + '*'*int(epoch)/5)
-    image_path = os.path.join("saved_models/LeNet_MNIST_Multi_4/images/inner_products_weights",
+    fig.suptitle(f"Histogram of Inner Products on Training Set" + '\n' + '*' * (int(epoch) // 5))
+    image_path = os.path.join("saved_models/LeNet_MNIST_Multi_3/images/inner_products_weights",
         f"checkpoint_{epoch}.pt.png",
     )
     plt.savefig(image_path)
